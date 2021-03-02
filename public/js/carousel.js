ce9be270edf8a4cf4bbfafaeac34835f08331fc7
tps1147 <tps1147@gmail.com>
2021-03-02 10:15:14 -0700

    commit to master

---
+++ public/js/carousel.js
@@ -0,0 +1,7 @@
+!(function(d){
+    var itemClassName = "carousel__slide";
+    items = d.getElementsByClassName(itemClassName),
+    totalItems = items.length,
+    slide = 0,
+    moving = true;
+  }(document));
